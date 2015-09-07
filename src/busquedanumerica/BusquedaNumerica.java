0ebf435eea573107d1318d02a4819fb704dca960
Flor Kaucic <florkaucic@gmail.com>
2015-09-07 10:40:18 -0300

    Commit inicial. Planteamos los metodos que tenemos que desarrollar

---
+++ src/busquedanumerica/BusquedaNumerica.java
@@ -0,0 +1,26 @@
+package busquedanumerica;
+
+public class BusquedaNumerica {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// hacer esto mas bonito
+		// leer el archivo
+		String n = "123456";
+		String m = "5346";
+		
+		int[] nvec;
+		int[] mvec = new int[10];
+				
+		for (int i = 0; i < m.length(); i++)
+			mvec[(int)(m.charAt(i))-48]++;
+		
+		/*
+		 * hacer la clase para los metodos:
+		 * constructor con otro vec
+		 * encontrar m en n
+		 * hacer mvec
+		 * leer el archivo
+		 * crear la salida
+		 */
+	}
+}
